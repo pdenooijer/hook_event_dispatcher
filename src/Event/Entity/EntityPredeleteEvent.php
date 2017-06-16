e0f0004f36978dde07903441a265abd3d0d27f5a
robin.ingelbrecht <robin.ingelbrecht@entityone.be>
2017-06-16 16:33:50 +0200

    Added support for hook_entity_predelete()

---
+++ src/Event/Entity/EntityPredeleteEvent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\hook_event_dispatcher\Event\Entity;
+
+use Drupal\hook_event_dispatcher\HookEventDispatcherEvents;
+
+/**
+ * Class EntityPredeleteEvent
+ * @package Drupal\hook_event_dispatcher\Event\Entity
+ */
+class EntityPredeleteEvent extends BaseEntityEvent {
+
+  /**
+   * @inheritdoc.
+   */
+  public function getDispatcherType() {
+    return HookEventDispatcherEvents::ENTITY_PRE_DELETE;
+  }
+}
