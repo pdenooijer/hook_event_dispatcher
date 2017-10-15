b39d2a7024313d98e0f9e0a39e7bf6c07eee0d42
Piet de Nooijer <piet@ibuildings.nl>
2017-10-15 21:58:26 +0200

    Add HookEventDispatcherManagerTest

---
+++ tests/src/Unit/Manager/FakeEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\hook_event_dispatcher\Unit\Manager;
+
+use Drupal\hook_event_dispatcher\Event\EventInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class FakeEvent.
+ *
+ * @package Drupal\Tests\hook_event_dispatcher\Unit\Manager
+ */
+class FakeEvent extends Event implements EventInterface {
+
+  /**
+   * Dispatcher type.
+   *
+   * @var string
+   */
+  private $dispatcherType;
+
+  /**
+   * FakeEvent constructor.
+   *
+   * @param string $dispatcherType
+   *   Dispatcher type.
+   */
+  public function __construct($dispatcherType) {
+    $this->dispatcherType = $dispatcherType;
+  }
+
+  /**
+   * Get the dispatcher type.
+   *
+   * @return string
+   *   The dispatcher type.
+   */
+  public function getDispatcherType() {
+    return $this->dispatcherType;
+  }
+
+}
---
+++ tests/src/Unit/Manager/HookEventDispatcherManagerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\hook_event_dispatcher\Unit\Manager;
+
+use Drupal\hook_event_dispatcher\Manager\HookEventDispatcherManager;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class HookEventDispatcherManagerTest.
+ *
+ * @package Drupal\Tests\hook_event_dispatcher\Unit\Manager
+ *
+ * @group hook_event_dispatcher
+ */
+class HookEventDispatcherManagerTest extends TestCase {
+
+  /**
+   * Test event dispatcher.
+   */
+  public function testEventDispatcher() {
+    $event = new FakeEvent('test');
+    $dispatcher = $this->createMock(EventDispatcherInterface::class);
+    $dispatcher->method('dispatch')->with('test', $event)->willReturn($event);
+
+    $manager = new HookEventDispatcherManager($dispatcher);
+    $returnedEvent = $manager->register($event);
+    $this->assertEquals($event, $returnedEvent);
+  }
+
+}
