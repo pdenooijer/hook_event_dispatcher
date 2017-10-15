4b6f3fff3f47e9a5302b53e04a84e4bab9dec97b
Piet de Nooijer <piet@ibuildings.nl>
2017-10-15 21:13:11 +0200

    Add TokenEventTest, TokenTest, TokenTypeTest

---
+++ tests/src/Unit/Token/TokenTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Tests\hook_event_dispatcher\Unit\Token;
+
+use Drupal\hook_event_dispatcher\Value\Token;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TokenTest.
+ *
+ * @package Drupal\Tests\hook_event_dispatcher\Unit\Token
+ */
+class TokenTest extends TestCase {
+
+  /**
+   * Test Token invalid type exception.
+   */
+  public function testTokenInvalidTypeException() {
+    $this->expectException(\UnexpectedValueException::class);
+    Token::create(NULL, '', '');
+  }
+
+  /**
+   * Test Token invalid token exception.
+   */
+  public function testTokenInvalidTokenException() {
+    $this->expectException(\UnexpectedValueException::class);
+    Token::create('', NULL, '');
+  }
+
+  /**
+   * Test Token invalid token exception.
+   */
+  public function testTokenInvalidNameException() {
+    $this->expectException(\UnexpectedValueException::class);
+    Token::create('', '', NULL);
+  }
+
+  /**
+   * Test Token invalid description exception.
+   */
+  public function testTokenInvalidDescriptionException() {
+    $this->expectException(\UnexpectedValueException::class);
+    Token::create('', '', '')->setDescription(NULL);
+  }
+
+}
