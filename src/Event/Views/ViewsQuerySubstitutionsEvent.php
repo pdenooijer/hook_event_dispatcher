772b7415dfe19c8823bb7d067ab7d577e0733eb6
Martin Keereman <mkeereman@tableau.com>
2019-07-05 17:04:22 -0700

    Setter and getter for query substitutions

--- src/Event/Views/ViewsQuerySubstitutionsEvent.php
+++ src/Event/Views/ViewsQuerySubstitutionsEvent.php
@@ -14,6 +14,39 @@ use Drupal\views\ViewExecutable;
 final class ViewsQuerySubstitutionsEvent extends BaseViewsEvent {
 
   /**
+   * Views query substitutions.
+   *
+   * @var array
+   */
+  private $substitutions;
+
+  /**
+   * Set the query substitutions.
+   *
+   * @param array $substitutions
+   *   An associative array where each key is a string to be replaced, and the
+   *   corresponding value is its replacement. The strings to replace are often
+   *   surrounded with '***', as illustrated in the example implementation, to
+   *   avoid collisions with other values in the query.
+   */
+  public function setSubstitutions(array $substitutions) {
+    $this->substitutions = $substitutions;
+  }
+
+  /**
+   * Get the query substitutions.
+   *
+   * @return array
+   *   An associative array where each key is a string to be replaced, and the
+   *   corresponding value is its replacement. The strings to replace are often
+   *   surrounded with '***', as illustrated in the example implementation, to
+   *   avoid collisions with other values in the query.
+   */
+  public function getSubstitutions() {
+    return $this->substitutions;
+  }
+
+  /**
    * Get the dispatcher type.
    *
    * @return string
