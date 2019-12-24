3d3b2315d580d1128b44423689ca2c38e7741b49
Ambient.Impact <i@ambientimpact.com>
2019-12-23 22:14:51 -0500

    Added ThemeRegistryAlterEvent.

---
+++ src/Event/Theme/ThemeRegistryAlterEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\hook_event_dispatcher\Event\Theme;
+
+use Drupal\hook_event_dispatcher\Event\EventInterface;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class ThemeRegistryAlterEvent.
+ */
+class ThemeRegistryAlterEvent extends Event implements EventInterface {
+
+  /**
+   * The entire cache of theme registry information, post-processing.
+   *
+   * @var array
+   */
+  private $themeRegistry;
+
+  /**
+   * ThemeRegistryAlterEvent constructor.
+   *
+   * @param array &$themeRegistry
+   *   The entire cache of theme registry information, post-processing.
+   */
+  public function __construct(array &$themeRegistry) {
+    $this->themeRegistry = &$themeRegistry;
+  }
+
+  /**
+   * Get theme registry.
+   *
+   * @return array
+   *   The theme registry.
+   */
+  public function &getThemeRegistry() {
+    return $this->themeRegistry;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDispatcherType() {
+    return HookEventDispatcherInterface::THEME_REGISTRY_ALTER;
+  }
+
+}
