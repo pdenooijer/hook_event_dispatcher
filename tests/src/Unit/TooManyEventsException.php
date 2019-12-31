a6292edc29a8147fdcf7f8e3dc00b29e091cf29e
Piet de Nooijer <Piet.de.Nooijer@ordina.nl>
2019-12-31 12:57:21 +0100

    Update HookEventDispatcherManagerInterface to use php7.1
    - Add return types
    - Boy scouting

--- tests/src/Unit/TooManyEventsException.php
+++ tests/src/Unit/TooManyEventsException.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\Tests\hook_event_dispatcher\Unit;
 
+use RuntimeException;
+
 /**
  * Class TooManyEventsException.
  *
  * @package Drupal\Tests\hook_event_dispatcher\Unit
  */
-class TooManyEventsException extends \RuntimeException {}
+class TooManyEventsException extends RuntimeException {}
