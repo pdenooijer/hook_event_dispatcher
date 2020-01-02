3cde5df7e859681772cf4531b3afc58b982148da
Matei Stanca <i@ambientimpact.com>
2020-01-02 15:45:18 -0500

    Add MediaSourceInfoAlterEvent
    This commit closes #99.

---
+++ modules/media_event_dispatcher/tests/src/Unit/Media/MediaSourceInfoAlterEventTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\Tests\media_event_dispatcher\Unit\Media;
+
+use Drupal;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\media_event_dispatcher\Event\Media\MediaSourceInfoAlterEvent;
+use Drupal\Tests\hook_event_dispatcher\Unit\HookEventDispatcherManagerSpy;
+use Drupal\Tests\UnitTestCase;
+use function media_event_dispatcher_media_source_info_alter;
+
+/**
+ * Class MediaSourceInfoAlterEventTest.
+ *
+ * @package Drupal\Tests\media_event_dispatcher\Unit\Media
+ *
+ * @group media_event_dispatcher
+ */
+class MediaSourceInfoAlterEventTest extends UnitTestCase {
+
+  /**
+   * The manager.
+   *
+   * @var \Drupal\Tests\hook_event_dispatcher\Unit\HookEventDispatcherManagerSpy
+   */
+  private $manager;
+
+  /**
+   * Sets up the test.
+   */
+  public function setUp() {
+    $builder = new ContainerBuilder();
+    $this->manager = new HookEventDispatcherManagerSpy();
+    $builder->set('hook_event_dispatcher.manager', $this->manager);
+    $builder->compile();
+    Drupal::setContainer($builder);
+  }
+
+  /**
+   * MediaSourceInfoAlterEvent sources array alter test.
+   *
+   * This tests altering the media source plugin definitions array.
+   */
+  public function testMediaSourceInfoAlter() {
+    $sources = $expectedSources = [
+      'image' => [
+        'id' => 'image',
+        'label' => new TranslatableMarkup('Image'),
+        'description' => new TranslatableMarkup('Use local images for reusable media.'),
+        'class' => 'Drupal\media\Plugin\media\Source\Image',
+        'allowed_field_types' => ['image'],
+      ],
+    ];
+
+    $this->manager->setEventCallbacks([
+      HookEventDispatcherInterface::MEDIA_SOURCE_INFO_ALTER => static function (
+        MediaSourceInfoAlterEvent $event
+      ) {
+        $sources = &$event->getSources();
+
+        $sources['image']['allowed_field_types'][] = 'test';
+      },
+    ]);
+
+    // Run the procedural hook which should trigger the above handler.
+    media_event_dispatcher_media_source_info_alter($sources);
+
+    /** @var \Drupal\media_event_dispatcher\Event\Media\MediaSourceInfoAlterEvent $event */
+    $event = $this->manager->getRegisteredEvent(
+      HookEventDispatcherInterface::MEDIA_SOURCE_INFO_ALTER
+    );
+
+    $this->assertSame($sources, $event->getSources());
+
+    $expectedSources['image']['allowed_field_types'][] = 'test';
+
+    $this->assertSame($expectedSources, $sources);
+  }
+
+}
--- src/HookEventDispatcherInterface.php
+++ src/HookEventDispatcherInterface.php
@@ -757,6 +757,18 @@ interface HookEventDispatcherInterface {
 
   // MEDIA EVENTS.
   /**
+   * Alters the information provided in \Drupal\media\Annotation\MediaSource.
+   *
+   * @Event
+   *
+   * @see media_event_dispatcher_media_source_info_alter()
+   * @see hook_media_source_info_alter()
+   *
+   * @var string
+   */
+  public const MEDIA_SOURCE_INFO_ALTER = 'hook_event_dispatcher.media.source_info_alter';
+
+  /**
    * Alters an oEmbed resource URL before it is fetched.
    *
    * @Event
