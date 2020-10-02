387883e2f68e843d0a51bb6d2d6833e0f4cea468
Jeroen Tubex <jeroen.tubex@intracto.com>
2020-10-02 17:38:23 +0200

    Add toolbar event

---
+++ modules/toolbar_event_dispatcher/tests/src/Unit/Toolbar/ToolbarEventTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\toolbar_event_dispatcher\Unit\Toolbar;
+
+use Drupal;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\Tests\hook_event_dispatcher\Unit\HookEventDispatcherManagerSpy;
+use Drupal\Tests\UnitTestCase;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\toolbar_event_dispatcher\Event\Toolbar\ToolbarEvent;
+use function toolbar_event_dispatcher_toolbar;
+
+/**
+ * Class ToolbarEventTest.
+ *
+ * @group toolbar_event_dispatcher
+ */
+class ToolbarEventTest extends UnitTestCase {
+
+  /**
+   * The manager.
+   *
+   * @var \Drupal\Tests\hook_event_dispatcher\Unit\HookEventDispatcherManagerSpy
+   */
+  private $manager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    $builder = new ContainerBuilder();
+    $this->manager = new HookEventDispatcherManagerSpy();
+    $builder->set('hook_event_dispatcher.manager', $this->manager);
+    $builder->compile();
+    Drupal::setContainer($builder);
+  }
+
+  /**
+   * Test the ToolbarAlterEvent.
+   */
+  public function testToolbarEvent(): void {
+    toolbar_event_dispatcher_toolbar();
+
+    $event = $this->manager->getRegisteredEvent(HookEventDispatcherInterface::TOOLBAR);
+
+    self::assertInstanceOf(ToolbarEvent::class, $event);
+  }
+
+}
